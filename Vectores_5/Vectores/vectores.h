60b0b12bdeb7f321cb394afdb57dc87b600f17be
D1kzu <agustinjaime0804@gmail.com>
2025-07-15 23:53:20 -0300

    AHORA SI ULTIMOS

--- Vectores_5/Vectores/vectores.h
+++
@@ -1,6 +0,0 @@
-#ifndef VECTORES_H_INCLUDED
-
-void InsertarElemento(int *vec,int *ce,int val,int TAM);
-void MostrarVector(int *vec,int ce);
-void InsertarPosicion(int *vec,int *ce,int val,int pos,int TAM);
-#endif // VECTORES_H_INCLUDED
